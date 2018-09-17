9d4e9cf95170571bce0483e4a25f8db1f60f5b89
sithsiri <sith@live.com>
2018-09-17 13:40:49 -0500

    more effort needed
    harder. faster. stronger. lighter. ~~longer battery life.~~

--- converter.py
+++ converter.py
@@ -1,2 +1,10 @@
 filename = "index.gs"
 rawText = open(filename, r)
+cache = str(rawText)
+convertCache = ""
+progress = (len(rawText)-len(cache))/len(rawText) #progress should always be this equation
+
+print(str(progress*100) + "% complete.") #print every so often
+
+while(len(cache) > 0):
+	pass
