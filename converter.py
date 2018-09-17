d3c45a8acb520e4867ca6bd6ae6fdf9669d345d6
sithsiri <sith@live.com>
2018-09-17 17:09:44 -0500

    syntax unscrew

--- converter.py
+++ converter.py
@@ -1,26 +1,26 @@
 filename = "index.gs"
-rawText = open(filename, r)
+rawText = open(filename, "r").read()
 cache = str(rawText)
 convertCache = ""
-progress = (len(rawText)-len(cache))/len(rawText) #progress should always be this equation
 
 def parseBuiltinConsole(input):
 	if(input[8:].startswith("print(")):
 		return(input[8:])
 
-print(str(progress*100) + "% complete.") #print every so often
-
 while(len(cache) > 0):
+	print(str(((len(rawText)-len(cache))/len(rawText))*100) + "% complete.") #print every so often
 	while(cache[1:] == "\n"):
 		cache = cache[1:]
 		convertCache = convertCache + "\n"
 	
-	if(cache.startswith("Console.")
+	if(cache.startswith("Console.")):
 		cacheNum = 0
 		while(cache[cacheNum] != ")"):
 			if(cache[cacheNum] == "\n"):
 				break #custom exception about how the close paren is missing
 		
-			cacheNum++
+			cacheNum = cacheNum + 1
 		
 		cache = cache + parseBuiltinConsole(cache[:cacheNum])
+
+open("index.py","w+").write(convertCache)
