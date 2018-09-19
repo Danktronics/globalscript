317078c1fb3859b6093537b6da8f75d73b5b27c4
sithsiri <sith@live.com>
2018-09-19 12:45:02 -0500

    comments work
    maybe multiline comments will be implemented, but for now, because some languages (👀) really hate them, so maybe later.

--- converter.py
+++ converter.py
@@ -2,6 +2,7 @@ filename = "index.gs"
 rawText = open(filename, "r").read()
 cache = str(rawText)
 convertCache = ""
+import sys
 
 def parseBuiltinConsole(parse):
 	if(parse[8:].startswith("print(")):
@@ -9,19 +10,31 @@ def parseBuiltinConsole(parse):
 	elif(parse[8:].startswith("println(")):
 		return("print(" + parse[16:])
 
+#start parsing
 while(len(cache) > 0):
-	while(len(cache) > 0 and cache[0] == "\n"):
+	while(len(cache) > 0 and cache[0] == "\n"):			#goes through newlines
 		cache = cache[1:]
 		convertCache = convertCache + "\n"
 	
-	if(cache.startswith("Console.")):
+	if(cache.startswith("Console.")):					#parses Console
 		cacheNum = 0
-		while(cache[cacheNum] != ")"):
-			if(cache[cacheNum] == "\n"):
-				break #custom exception about how the close paren is missing
-			else: cacheNum = cacheNum + 1
+		while(len(cache)>0 and cache[cacheNum]!=")"):	#finds close parenthesis
+			if(cache[cacheNum] == "\n"):				#quits on newline
+				exit() #exception about how the close paren is missing
+			if(cache[cacheNum:cacheNum+1] == "//"):		#quits on comment
+				exit() #similar exception to above
+			cacheNum = cacheNum + 1
 		cacheNum = cacheNum + 1
 		convertCache = convertCache + parseBuiltinConsole(cache[:cacheNum])
+		cache = cache[cacheNum:]
+	
+	while(cache.startswith("//")):
+		cacheNum = 0
+		cache = cache[2:]
+		convertCache = convertCache + "#"
+		while(len(cache) > 0 and cache[cacheNum] != "\n"):
+			cacheNum = cacheNum + 1
+		convertCache = convertCache + cache[:cacheNum]
 		cache = cache[cacheNum:]
 	
 	print(str(((len(rawText)-len(cache))/len(rawText))*100) + "% complete.")
