cd3c275bdf922c69dff38d1187360876cfb61145
sithsiri <sith@live.com>
2018-09-21 16:01:14 -0500

    added command line arguments
    quick tutorial:
    running just `globalscript.py` will convert from index.gs to index.py
    running `globalscript.py filename.gs` will convert from filename.gs to filename.py
    running `globalscript.py filename.gs output.py` will convert from filename.gs to output.py
    if the first argument isn't a .gs file or the second argument exists and isn't a .py file, the converter will ask for confirmation before starting.

--- globalscript.py
+++ globalscript.py
@@ -1,14 +1,57 @@
-filename = "index.gs"
-rawText = open(filename, "r").read()
-cache = str(rawText)
-convertCache = ""
+import os
 import sys
+import math
 
 def parseBuiltinConsole(parse):
 	if(parse[8:].startswith("print(")):
 		return(parse[8:len(parse)-1] + ", end='', flush=True)")
 	elif(parse[8:].startswith("println(")):
 		return("print(" + parse[16:])
+
+isPython = True
+
+if(len(sys.argv) == 1): #default if no args are passed, Python will autobrick
+	filename = "index.gs"
+	outputname = "index.py"
+elif(len(sys.argv) == 2): #1 arg is passed
+	if(sys.argv[1].endswith(".gs") and os.path.isfile(sys.argv[1])):
+		filename = sys.argv[1]
+		outputname = sys.argv[1][:3] + ".py"
+	elif(os.path.isfile(sys.argv[1])):
+		if(input("The specified file isn't a .gs file. Continue anyways? (y/anything else)").startswith("y")):
+			filename = sys.argv[1]
+			outputname = sys.argv[1][:3] + ".py"
+		else:
+			sys.exit()
+	else:
+		open(sys.argv[1], "r") #should throw exception and brick
+		sys.exit()
+elif(len(sys.argv) == 3): #2 args are passed
+	if(sys.argv[1].endswith(".gs") and os.path.isfile(sys.argv[1])):
+		filename = sys.argv[1]
+	elif(os.path.isfile(sys.argv[1])):
+		if(input("The specified input file isn't a .gs file. Continue anyways? (y/anything else)").startswith("y")):
+			filename = sys.argv[1]
+		else:
+			sys.exit()
+	else:
+		open(sys.argv[1], "r") #should throw exception and brick
+		sys.exit()
+	
+	if(sys.argv[2].endswith(".py") and os.path.isfile(sys.argv[2])):
+		outputname = sys.argv[2]
+	elif(os.path.isfile(sys.argv[2])):
+		if(input("The specified output file isn't a .py file. Continue anyways? (y/anything else)").startswith("y")):
+			outputname = sys.argv[2]
+			isPython = False
+		else:
+			sys.exit()
+	else:
+		open(sys.argv[2], "r") #should throw exception and brick
+		sys.exit()
+rawText = open(filename, "r").read()
+cache = str(rawText)
+convertCache = ""
 
 #start parsing
 while(len(cache) > 0):
@@ -42,10 +85,11 @@ while(len(cache) > 0):
 		convertCache = convertCache + cache[:cacheNum]
 		cache = cache[cacheNum:]
 	
-	print(str(((len(rawText)-len(cache))/len(rawText))*100) + "% complete.")
+	print(str(math.floor(((len(rawText)-len(cache))/len(rawText))*100)) + "% complete.")
 
 print("done, saving...")
 open("index.py","w+").write(convertCache)
 print("saved to index.py")
-print("running globalscript code: \n")
-import index
+if(isPython or input("Didn't run output file because it isn't a `.py` file. Try anyways? (y/anything else)").startswith("y")):
+	print("running output file: \n")
+	import index
